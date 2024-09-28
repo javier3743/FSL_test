5de0e92830620846d098910f892edb4a139b81b6
Javier Palacios <javierpala2020@gmail.com>
2024-09-28 13:39:08 -0500

    Cloudfront

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -15,33 +15,6 @@ resource "aws_s3_bucket_website_configuration" "app_bucket" {
 
 }
 
-resource "aws_s3_bucket_public_access_block" "app_bucket" {
-  bucket = aws_s3_bucket.app_bucket.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_policy" "app_bucket" {
-  bucket = aws_s3_bucket.app_bucket.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.app_bucket.arn}/*"
-      },
-    ]
-  })
-  depends_on = [ aws_s3_bucket_public_access_block.app_bucket ]
-}
-
 resource "aws_s3_object" "build_files" {
   for_each = fileset("../../../build", "**/*")
 
@@ -62,4 +35,65 @@ locals {
     ".json" = "application/json"
     ".txt"  = "text/plain"
   }
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.app_bucket.bucket_regional_domain_name
+    origin_id   = "S3-${var.bucket_name}"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-${var.bucket_name}"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  depends_on = [ aws_s3_object.build_files ]
+}
+
+resource "aws_s3_bucket_policy" "app_bucket" {
+  bucket = aws_s3_bucket.app_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AllowCloudFrontAccess"
+        Effect    = "Allow"
+        Principal = {
+          AWS = aws_cloudfront_distribution.s3_distribution.arn
+        }
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.app_bucket.arn}/*"
+      },
+    ]
+  })
+  depends_on = [aws_cloudfront_distribution.s3_distribution]
 }
